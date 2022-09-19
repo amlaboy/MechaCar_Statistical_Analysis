ab5e370f5e7e8937187a5d2ff46318d32a407956
Antonio <amlaboy96@gmail.com>
2022-09-19 00:05:40 -0400

    added d3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,23 @@ total_summary <- summarize(coil_table, Mean = mean(PSI), Median = median(PSI), V
 #lot_grouping <- coil_table <- group_by(Manufacturing_Lot, PSI)
   
 lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean =mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI),  .groups = 'keep')
+
+# Randomly sample 50 data points
+sample_table <- coil_table %>% sample_n(50)
+
+# T-test
+t.test(log10(sample_table$PSI),mu=mean(log10(coil_table$PSI)))
+
+# Create subsets and test lot 1
+lot_one <- subset(coil_table, Manufacturing_Lot == "Lot1" ) 
+t.test(log10(lot_one$PSI),mu=mean(log10(coil_table$PSI)))
+
+# Create subsets and test lot 2
+lot_two <- subset(coil_table, Manufacturing_Lot == "Lot2" ) 
+t.test(log10(lot_two$PSI),mu=mean(log10(coil_table$PSI)))
+
+# Create subsets and test lot 2
+lot_three <- subset(coil_table, Manufacturing_Lot == "Lot3" ) 
+t.test(log10(lot_three$PSI),mu=mean(log10(coil_table$PSI)))
+
+

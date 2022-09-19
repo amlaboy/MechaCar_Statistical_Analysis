71724078a2c2fd1665399bad4ffae3358791c075
Antonio <amlaboy96@gmail.com>
2022-09-18 21:48:23 -0400

    Added D2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,21 @@
 # Import Libraries
 library(tidyverse)
-# Create Df
+# Create mecha car table
 mecha_car_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 # Create LM
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , data = mecha_car_table)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , data = mecha_car_table))
+
+
+# Create suspension coil table
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create total summary
+total_summary <- summarize(coil_table, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+##
+#lot_grouping <- coil_table <- group_by(Manufacturing_Lot, PSI)
+  
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean =mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI),  .groups = 'keep')
